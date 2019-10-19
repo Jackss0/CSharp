f7028b2a524b8f6834f455b4f7378a496b7f22c9
JksHub <34433958+Jackss0@users.noreply.github.com>
2019-10-19 14:20:08 -0500

    first commit

---
+++ CSharp6/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Using static
+using static CSharp6.CSharp6;
+using static CSharp6.CSharp6.OtherClass;
+
+namespace CSharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstName);
+            Console.WriteLine(FullName);
+            SomeMethod();
+            int n = FullName.CountWords();
+            int n2 = FullName.CountWords() + 2;
+            Console.WriteLine("There's {0} {1} words", n, n2);
+
+            //nameof expression
+            Console.WriteLine(nameof(FirstName));
+        }
+    }
+
+    static class CSharp6
+    {
+        //Read-only auto-properties
+        public static string FirstName { get; } = "Josue";
+        public static string LastName { get; set; } = "Tueros";
+
+        static void GetProp()
+        {
+            //Cannot access to this prop, cause is read-only
+            //FirstName = "xd";
+
+        }
+
+        //Auto-property initializers
+        public static ICollection<string> Courses = new List<string>();
+
+        //Props with body expression
+        public static string FullName => $"{FirstName} {LastName}";
+
+        public static int CountWords(this string text)
+        {
+            return text.Split(' ').Length;
+        }
+
+        public class OtherClass
+        {
+            public string MyProperty { get; set; }
+
+            //NULL conditional operator
+            public static void SomeMethod()
+            {
+                LastName = null;
+                Console.WriteLine(LastName?.ToUpper());
+
+                var list = new List<string>
+                {
+                    "1",
+                    "2",
+                    null
+                };
+                Console.WriteLine(list?[2]);
+                Console.WriteLine(LastName?.ToUpper() ?? "default value");
+                Console.WriteLine(list?[2] ?? "default list value");
+
+                //String interpolation
+                Console.WriteLine($"FullName: {FirstName} + {LastName}");
+                
+            }
+        }
+        
+    }
+}
